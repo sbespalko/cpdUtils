c1e4e94ad871a4cd2712ef399b570c9614260918
sbespalko <W9nC^*Df&ONd>
2017-12-05 23:44:17 +0300

    work, but I not sure about good decision it

--- src/main/java/cpd/utils/CpdUtilsApplication.java
+++ src/main/java/cpd/utils/CpdUtilsApplication.java
@@ -4,7 +4,9 @@ import cpd.utils.controller.CsvExporter11;
 import cpd.utils.controller.XmlExporter11;
 import cpd.utils.model.FileSystemModel11;
 import cpd.utils.model.Model;
+import cpd.utils.transformer.CsvTransformer11;
 import cpd.utils.transformer.Transformer;
+import cpd.utils.transformer.XmlTransformer11;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,28 +18,18 @@ public class CpdUtilsApplication {
     SpringApplication.run(CpdUtilsApplication.class, args);
   }
 
-  private Model model11;
-  private Transformer csvTransformer11;
-  private Transformer xmlTransformer11;
-
-  public CpdUtilsApplication(Transformer csvTransformer11, Transformer xmlTransformer11) {
-    this.model11 = model11();
-    this.csvTransformer11 = csvTransformer11;
-    this.xmlTransformer11 = xmlTransformer11;
-  }
-
   @Bean
   public Model model11() {
-    return new FileSystemModel11(xmlTransformer11);
+    return new FileSystemModel11(new XmlTransformer11());
   }
 
   @Bean
   public XmlExporter11 xmlExporter11() {
-    return new XmlExporter11(model11, xmlTransformer11);
+    return new XmlExporter11(model11(), new XmlTransformer11());
   }
   @Bean
   public CsvExporter11 csvExporter11() {
-    return new CsvExporter11(model11, csvTransformer11);
+    return new CsvExporter11(model11(), new CsvTransformer11());
   }
 
 }
